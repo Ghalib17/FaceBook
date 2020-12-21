312378abbfeafb161ece026bb47afa0ec1a76c81
Ghalib Cherif <ghalebs17@gmail.com>
2020-12-21 16:46:02 +0100

    new version 16:45

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('Hello Cancun travel cancelled')
+a=2
+b=5
+print('initial value')
+print('a= ', ' and b = ', b)
+c = b 
+b = a 
+a = c 
+print ('swiped values')
+print ('a= ', a, ' and b = ', b)
